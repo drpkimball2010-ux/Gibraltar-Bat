5f026ab67c910f36090b0e73b504c49b1bb82470
Danny Kimball <dannykimball@Dannys-MacBook-Air.local>
2026-05-21 19:20:31 -0500

    Add debug endpoint; show real errors in checkout

---
+++ api/debug.js
@@ -0,0 +1,9 @@
+module.exports = function (req, res) {
+  const key = process.env.STRIPE_SECRET_KEY;
+  res.status(200).json({
+    has_key: !!key,
+    key_length: key ? key.length : 0,
+    key_prefix: key ? key.slice(0, 7) : null,
+    node_version: process.version,
+  });
+};
